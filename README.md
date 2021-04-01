5bc7293629a8980162e28e0fd7da5060209c9b4f
Michael Dominguez <michaelmason579@gmail.com>
2021-04-01 12:39:10 -0500

    Added an ETL script, test data and modified the README

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-Respose to ORNL API Exercise
+<h1>Respose to ORNL API Exercise</h1>
+there are two files: etl.py and server.py
+
+<h2>etl.py</h2>
+This script gets passed a csv file through the terminal <code>etl.py < example\_batch\_records.csv</code>, then it will remove any datasets that are not complete. Finaly, it will load the vetted data into a MySQL DB.
+
+This file will not check if the data is an integer, or ISO date.
+
+<h2>server.py</h2>
+TODO
---
+++ etl.py
@@ -0,0 +1,30 @@
+# This script will get passed a .csv file through the terminal
+# then it will take out any rows missing data. Finally, it will
+# load the corected data into a MySQL DB.
+import sys
+
+def get_data():
+    # This is the array to send to MySQL database
+    fout = []
+
+    for line in sys.stdin:
+        line = line.strip();
+        check_element = line.split(',')
+        add_line = True
+
+        # check to see if any elements are missing, if they are
+        # dont add the line to the new csv file 
+        for element in check_element:
+            if element == '':
+                add_line = False
+
+        if add_line:
+            fout.append(line)
+    return fout
+
+# This should connect to the DB and populate it with the new csv
+def push_data():
+    pass
+
+if __name__ = '__main__':
+    push_data(get_data())
